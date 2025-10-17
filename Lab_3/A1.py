9359a88c9da172e910a91736dfb8a4e24687d106
Сытая Яна <sytay_ys_25@student.grsu.by>
2025-10-17 12:41:50 +0300

    Add files via upload

---
+++ Lab_3/A1.py
@@ -0,0 +1,14 @@
+x1 = float(input("x1: "))
+y1 = float(input("y1: "))
+x2 = float(input("x2: "))
+y2 = float(input("y2: "))
+if (x1 > 0 and y1 > 0) and (x2 > 0 and y2 > 0):
+    print("Yes,I")
+elif (x1 < 0 and y1 > 0) and (x2 < 0 and y2 > 0):
+    print("Yes,II")
+elif (x1 < 0 and y1 < 0) and (x2 < 0 and y2 < 0):
+    print("Yes,III")
+elif (x1 > 0 and y1 < 0) and (x2 > 0 and y2 < 0):
+    print("Yes,IV")
+else:
+    print("No")
